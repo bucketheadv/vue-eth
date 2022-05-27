e4327a50ee29b46cd53d7fc31d6295116184b55c
qinglinl <qinglinl@rings.net.cn>
2022-05-27 16:58:37 +0800

    add vue-dapp

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <button @click="open">Connect Wallet</button>
+  <vd-board :connectors="connectors" />
   <router-view/>
 </template>
 
@@ -28,3 +30,34 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+import { CoinbaseWalletConnector, MetaMaskConnector, useBoard, WalletConnectConnector } from 'vue-dapp';
+export default {
+  name: 'App',
+  setup() {
+    const { open } = useBoard();
+    const infuraId = "";
+    const connectors = [
+      new MetaMaskConnector({
+        appUrl: "http://localhost:3000",
+      }),
+      new WalletConnectConnector({
+        qrcode: true,
+        rpc: {
+          1: `https://mainnet.infura.io/v3/${infuraId}`,
+          4: `https://rinkeby.infura.io/v3/${infuraId}`,
+        },
+      }),
+      new CoinbaseWalletConnector({
+        appName: "Vue Dapp",
+        jsonRpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
+      }),
+    ];
+    return {
+      connectors,
+      open,
+    };
+  }
+}
+</script>
